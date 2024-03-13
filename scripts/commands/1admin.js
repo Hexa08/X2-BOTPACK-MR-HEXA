718abd4bdbf34b3915bd370ae19531954b957ac8
Hexa08 <159428338+Hexa08@users.noreply.github.com>
2024-03-14 00:59:52 +0600

    1admin.js

--- scripts/commands/1admin.js
+++ scripts/commands/1admin.js
@@ -29,7 +29,7 @@ var link =[
             "https://i.imgur.com/N2tNu0u.jpg"
 					];
   
-var callback = () => api.sendMessage({body:`𝗡𝗮𝗺𝗲       : Madhab Datta\n𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : Madhab\n𝗥𝗲𝗹𝗶𝗴𝗶𝗼𝗻    : Hindu\n𝗣𝗲𝗿𝗺𝗮𝗻𝗲𝗻𝘁 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : ❤️‍🔥Chattogram🔥\n𝗖𝘂𝗿𝗿𝗲𝗻𝘁 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : NO NEED TO KNOW \n𝗚𝗲𝗻𝗱𝗲𝗿     : Male\n𝗔𝗴𝗲            :  21\n𝗪𝗼𝗿𝗸         :  Student  \n𝗚𝗺𝗮𝗶𝗹        :  hamimhosenx@gmail.com\n𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 :  wa.me/+8801876401888\n𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺  : X2_IS_HERE\n𝗙𝗯 𝗹𝗶𝗻𝗸   : https://www.facebook.com/HACKER.HAMIM.BHAI
+var callback = () => api.sendMessage({body:`𝗡𝗮𝗺𝗲       : Madhab Datta\n𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : Madhab\n𝗥𝗲𝗹𝗶𝗴𝗶𝗼𝗻    : Hindu\n𝗣𝗲𝗿𝗺𝗮𝗻𝗲𝗻𝘁 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : ❤️‍🔥Chattogram🔥\n𝗖𝘂𝗿𝗿𝗲𝗻𝘁 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : NO NEED TO KNOW \n𝗚𝗲𝗻𝗱𝗲𝗿     : Male\n𝗔𝗴𝗲            :  21\n𝗪𝗼𝗿𝗸         :  Student  \n𝗚𝗺𝗮𝗶𝗹        :madhabdatta07@gmail.com\n𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 :  wa.me/+8801897769514\n𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺  : \n𝗙𝗯 𝗹𝗶𝗻𝗸   :https://www.facebook.com/share/RHQj8Y7imu9tncqJ/?mibextid=qi2Omg
 `,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
    };
